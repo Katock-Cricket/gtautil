baf8ef6aa0c7c9f7e028fdc4adf40df8298d3058
GiZz <gizzdev@proton.me>
2023-03-17 10:00:05 +0100

    do not provide cache.json in dist because it is based on user installation and modifications of GTA5

--- make-dist.ps1
+++ make-dist.ps1
@@ -1,5 +1,4 @@
 7z a -tzip gtautil-0.0.0.zip `
     .\gtautil\bin\x64\Release\*.exe `
     .\gtautil\bin\x64\Release\*.dll `
-    .\gtautil\bin\x64\Release\cache.json `
     .\gtautil\bin\x64\Release\strings.txt
