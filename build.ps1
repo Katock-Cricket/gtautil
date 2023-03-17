bdcb76f4f634377ce9dfebc76ed403befc3329ff
GiZz <gizzdev@proton.me>
2023-03-17 09:48:48 +0100

    update build

--- build.ps1
+++ build.ps1
@@ -1,35 +1,56 @@
-param (
-    [string]$build= "Release"
+Param (
+  [string]$build= "Release"
 )
 
-Function Find-MSBuild([int] $MaxVersion = 2019)
+Install-Module VSSetup -Scope CurrentUser -Force
+
+function Find-MSBuild
 {
-    $agentPath2019     = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
-    $devPath2019       = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
-    $proPath2019       = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe"
-    $communityPath2019 = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
-    $agentPath2017     = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
-    $devPath2017       = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
-    $proPath2017       = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe"
-    $communityPath2017 = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
-    $fallback2015Path  = "${Env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
-    $fallback2013Path  = "${Env:ProgramFiles(x86)}\MSBuild\12.0\Bin\MSBuild.exe"
-    $fallbackPath      = "C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+  Param 
+  (
+    [bool] $allowPreviewVersions = $false
+  )
+    if ($allowPreviewVersions) {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All -Prerelease | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    } else {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    }
+    Write-Host "Latest version installed is $($latestVsInstallationInfo.InstallationVersion)"
+    if ($latestVsInstallationInfo.InstallationVersion -like "15.*") {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\15.0\Bin\msbuild.exe"
     
-    If ((2019 -le $MaxVersion) -And (Test-Path $agentPath2019))     { return $agentPath2019 } 
-    If ((2019 -le $MaxVersion) -And (Test-Path $devPath2019))       { return $devPath2019 } 
-    If ((2019 -le $MaxVersion) -And (Test-Path $proPath2019))       { return $proPath2019 } 
-    If ((2019 -le $MaxVersion) -And (Test-Path $communityPath2019)) { return $communityPath2019 } 
-    If ((2017 -le $MaxVersion) -And (Test-Path $agentPath))         { return $agentPath2017 } 
-    If ((2017 -le $MaxVersion) -And (Test-Path $devPath))           { return $devPath2017 } 
-    If ((2017 -le $MaxVersion) -And (Test-Path $proPath))           { return $proPath2017 } 
-    If ((2017 -le $MaxVersion) -And (Test-Path $communityPath))     { return $communityPath2017 } 
-    If ((2015 -le $MaxVersion) -And (Test-Path $fallback2015Path))  { return $fallback2015Path } 
-    If ((2013 -le $MaxVersion) -And (Test-Path $fallback2013Path))  { return $fallback2013Path } 
+      Write-Host "Located msbuild for Visual Studio 2017 in $msbuildLocation"
+    } else {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\Current\Bin\msbuild.exe"
+      Write-Host "Located msbuild in $msbuildLocation"
+    }
+
+    return $msbuildLocation
+}
+
+$MSBuild = Find-MSBuild
+function Find-MSBuild
+{
+  Param 
+  (
+    [bool] $allowPreviewVersions = $false
+  )
+    if ($allowPreviewVersions) {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All -Prerelease | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    } else {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    }
+    Write-Host "Latest version installed is $($latestVsInstallationInfo.InstallationVersion)"
+    if ($latestVsInstallationInfo.InstallationVersion -like "15.*") {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\15.0\Bin\msbuild.exe"
     
-    If (Test-Path $fallbackPath) { return $fallbackPath } 
-        
-    throw "Unable to find MSBuild"
+      Write-Host "Located msbuild for Visual Studio 2017 in $msbuildLocation"
+    } else {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\Current\Bin\msbuild.exe"
+      Write-Host "Located msbuild in $msbuildLocation"
+    }
+
+    return $msbuildLocation
 }
 
 $MSBuild = Find-MSBuild
